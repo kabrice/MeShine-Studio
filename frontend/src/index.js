1a6d1f17184de29825972014a8faa06b4c705a56
Edgar KAMDEM <briceouabo@gmail.com>
2017-11-29 20:07:20 +0100

    Initalisation of the webpack configuration

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,14 @@
-console.log("hi");
-import '../node_modules/bootstrap/dist/js/bootstrap.js';
-import '../css/main.css';
+// Bootstrap dependencies
+window.$ = window.jQuery = require('jquery') // required for bootstrap
+window.Popper = require('popper.js') // required for tooltip, popup...
+import 'bootstrap'
+import '../scss/main.scss'
 
+import './index.scss' // include bootstrap css file with own modifications
+
+// tooltip and popover require javascript side modification to enable them (new in Bootstrap 4)
+// use tooltip and popover components everywhere
+$(function (){
+    $('[data-toggle="tooltip"]').tooltip();
+    $('[data-toggle="popover"]').popover();
+})
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,42 +1,54 @@
-const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const webpack = require('webpack');
+const webpack = require('webpack')
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
+const path = require('path')
 
-const VENDOR_LIBS = ['bootstrap'];
+const extractSass = new ExtractTextPlugin({
+    filename: "styles.css",
+});
+
 
 module.exports = {
-    entry: {
-        bundle: './src/index.js',
-        vendor: VENDOR_LIBS
-    },
+    entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: '[name].js'
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'build')
     },
     module: {
         rules: [
             {
-                use: 'babel-loader',
-                test: /\.js$/
+                test: /\.js$/,
+                include: path.resolve(__dirname, 'src'),
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['es2015', { modules: false }]
+                        ]
+                    }
+                }]
             },
             {
-                loader: ExtractTextPlugin.extract({
-                    use: 'css-loader'
-                }),
-                test: /\.css$/
+                test: /\.(scss)$/,
+                use: extractSass.extract({
+                    fallback: 'style-loader',
+                    //resolve-url-loader may be chained before sass-loader if necessary
+                    use: [{
+                        loader: "css-loader" // translates CSS into CommonJS
+                    }, {
+                        loader: "sass-loader" // compiles Sass to CSS
+                    }]
+                })
             },
 
         ]
     },
     plugins: [
-        new ExtractTextPlugin('style.css'),
         new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery',
-            'window.jQuery': 'jquery',
+            $: "jquery", // Used for Bootstrap JavaScript components
+            jQuery: "jquery", // Used for Bootstrap JavaScript components
             Tether: 'tether',
-            Popper: ['popper.js', 'default']
-        })
+            Popper: ['popper.js', 'default'] // Used for Bootstrap dropdown, popup and tooltip JavaScript components
+        }),
+        extractSass
     ]
 };
-
