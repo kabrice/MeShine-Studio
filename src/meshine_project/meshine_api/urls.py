ec4e03264db2b7b31cdda9da0130cb212477aafb
Edgar KAMDEM <briceouabo@gmail.com>
2018-01-04 23:03:35 +0100

    Added profile listing and creating feature

--- src/meshine_project/meshine_api/urls.py
+++ src/meshine_project/meshine_api/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 router = DefaultRouter()
 router.register('Hello-viewset', views.HelloViewSet, base_name='hello-viewset')
+router.register('profile', views.UserProfileViewSet)
 
 urlpatterns= [
     path('hello-view/', views.HelloApiView.as_view()),
