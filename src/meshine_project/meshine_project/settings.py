03b5ff0de38da2d01b114a267559c778200db110
Edgar KAMDEM <briceouabo@gmail.com>
2018-01-06 22:25:12 +0100

    Switch from sqlite to postgresql

--- src/meshine_project/meshine_project/settings.py
+++ src/meshine_project/meshine_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'psycopg2',
     'rest_framework',
     'rest_framework.authtoken',
     'meshine_api',
@@ -78,8 +79,12 @@ WSGI_APPLICATION = 'meshine_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'meshine',
+        'USER': 'meshine',
+        'PASSWORD': 'meshine',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
