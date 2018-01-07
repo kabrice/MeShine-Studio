8af341ff228fbe34466f4a9b8620a7fd0d5bd5ca
Edgar KAMDEM <briceouabo@gmail.com>
2018-01-07 03:08:36 +0100

    Added model for Summary and UserSummary, created migration and added to django admin

--- src/meshine_project/meshine_api/models.py
+++ src/meshine_project/meshine_api/models.py
@@ -59,3 +59,20 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
         """Django uses this when it needs to convert the object to a string"""
 
         return self.email
+
+class Summary(models.Model):
+
+    question_text = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.question_text
+
+class UserSummary(models.Model):
+    """Manytomany table"""
+    user_profile = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
+    summary = models.ForeignKey('Summary', on_delete=models.CASCADE)
+    is_author = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
